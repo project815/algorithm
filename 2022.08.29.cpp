a7d351a0de49e30900619907a370a90736cf2602
Minseok Song <ske3056@gmail.com>
2022-08-29 18:44:26 +0900

    14916 / 2022.08.29

---
+++ 2022.08.29.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	int cnt = 0;
+	cin >> N;
+
+	while (!(N % 5 == 0) && N > 0)
+	{
+		if (N == 1 || N == 3) break;
+		N -= 2;
+		cnt++;
+	}
+	if ((N == 1 || N == 3) && N < 4)
+	{
+		cout << "-1";
+	}
+	else
+	{
+		cout << N / 5 + cnt;
+	}
+	
+	return 0;
+}
