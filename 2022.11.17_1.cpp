406ed9e5f3a6fbdae5252ebb5249d8e58fcb580b
Song minseok <ske3056@gmail.com>
2022-11-17 23:31:27 +0900

    bronze1

---
+++ 2022.11.17_1.cpp
@@ -0,0 +1,19 @@
+/*
+11.17 알고리즘 문제 풀기 하나씩 또는
+쉬운 문제면 여러개
+단, 꾸준히 한 문제 이상씩은 풀 것.
+*/
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    double a, b;
+    cin >> a >> b;
+
+    cout.fixed;
+    cout.precision(10);
+    cout << a / b << endl;
+
+    return 0;
+}
