8e9dd4df80cee2df96ef023b0ef0966eea5e8157
Song minseok <ske3056@gmail.com>
2022-11-17 23:46:39 +0900

    add

---
+++ 2022.11.17_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(void)
+{
+    long long int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a + b + c<< endl;
+    return 0;
+}
