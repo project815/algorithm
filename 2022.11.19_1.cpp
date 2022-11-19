57f31677c2ec51d4c4b25c030f820e68eeb82133
Song minseok <ske3056@gmail.com>
2022-11-19 14:11:47 +0900

    correct

--- 2022.11.19_1.cpp
+++ 2022.11.19_1.cpp
@@ -6,23 +6,42 @@ int main(void)
     int a, b;
     cin >> a >> b;
 
+    int min = a < b ? a : b;
+
+    int i = 1;
     int max_common_divisor = 1;
     int min_common_divisor = 1;
 
-    for(int i = 1; i <= 10000; i++)
+    while(i <= min)
     {
         if(a % i == 0 && b % i == 0)
         {
-            a /= i;
-            b /= i;
-            max_common_divisor *= i; 
+            max_common_divisor = i; 
         }
-        else
-        {
-            min_common_divisor = max_common_divisor * a * b;
-        }
-    }
 
-    cout << max_common_divisor << " " << min_common_divisor;
+        i+=1;
+    }
+    cout << max_common_divisor << endl;
+    cout << (a * b) / max_common_divisor << endl; 
     return 0;
 }
+/*
+24 18이 주어졌을 때
+2|24 18
+--------
+3|12 9
+--------
+   4 3
+
+최대공약수는 2 * 3 * 4 * 3 = 72
+최소공약수는 2 * 3 = 6이다.
+
+2 * 3 * 4 = 24
+2 * 3 * 3 = 18
+
+따라서 24 * 18 = (2 * 3 * 4) * (2 * 3 * 3)이다.
+즉, 최대공약수 * 최소공약수 = 24 *18을 의미한다.
+
+최대공약수를 구한 뒤 (a * b) / 최대공약수를 하면 최소공약수의 값을 구할 수 있다.
+
+*/
