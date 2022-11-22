8b62d503c1c39394521f522d3bb6435c4028f04e
Song minseok <ske3056@gmail.com>
2022-11-22 23:45:49 +0900

    11.22

---
+++ 2022.11.21_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+/*
+백준 1018번 문제
+*/
+using namespace std;
+int main(void)
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    vector <int> arr;
+    
+    arr.resize(a * b);
+
+
+    return 0;
+}
