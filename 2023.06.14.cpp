0b6771cf98a4881f55b663ea25f6e577b25840c9
songminseok <ske3056@gmail.com>
2023-06-14 00:25:26 +0900

    2023.06.14

---
+++ 2023.06.14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+//백준 1330번: 두 수 비교하기
+
+using namespace std;
+int main(void)
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    string str = a > b ? ">" : a < b ? "<" : "==";
+
+    cout << str;
+
+    return 0;
+}
